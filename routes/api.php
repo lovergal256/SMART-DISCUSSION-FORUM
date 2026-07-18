1a3853ec203be57a2d4ce5776191d0ec3458e80e
Jeremiah Mukisa <dxwll.504@gmail.com>
2026-07-18 09:11:36 +0300

    Add REST API: Sanctum auth, group endpoints for desktop app.

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthApiController;
+use App\Http\Controllers\Api\GroupApiController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/login', [AuthApiController::class, 'login']);
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/logout', [AuthApiController::class, 'logout']);
+
+    // Group endpoints
+    Route::get('/groups', [GroupApiController::class, 'index']);
+    Route::post('/groups', [GroupApiController::class, 'store']);
+    Route::get('/groups/discover', [GroupApiController::class, 'discover']);
+    Route::get('/groups/{id}', [GroupApiController::class, 'show']);
+});
